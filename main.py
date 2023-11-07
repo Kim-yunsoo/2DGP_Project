5b05fb9a294285edc747527adc9a3e64289f58af
Kim Yun Soo <dev.yunsoo@gmail.com>
2023-11-07 17:45:08 +0900

    main 수정

---
+++ main.py
@@ -0,0 +1,11 @@
+from pico2d import open_canvas, delay, close_canvas
+import game_framework
+# import logo_mode as start_mode
+import title_mode as start_mode
+# import play_mode as start_mode
+# import select_mode as start_mode
+
+
+open_canvas(1000 ,700)
+game_framework.run(start_mode)
+close_canvas()
